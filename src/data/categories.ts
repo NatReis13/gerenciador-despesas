c80621cde1e55eee31d99c8e29f64e6ec39cd252
NatReis13 <natan-12rs@hotmail.com>
2022-08-15 14:36:58 -0300

    net

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,8 +1,7 @@
 import { Category } from "../types/Category"
 
 export  const categories: Category = {
-food: { title: 'Alimentação', color: 'blue', expense: true},
-rent: {title: 'Aluguel', color: 'brown', expense: true},
-salary: {title: 'Salário', color: 'green', expense: false},
+expense: {title: 'Despesa', color: 'brown', expense: true},
+received: {title: 'Recebido', color: 'green', expense: false},
 
 }
--- src/data/items.ts
+++ src/data/items.ts
@@ -1,8 +1,4 @@
 import { Item } from "../types/Item";
 
 export const items: Item[] = [
-    {date: new Date(2022, 7, 5), category:'food', title:'McDonalds', value:32.12},
-    {date: new Date(2022, 7, 5), category:'food', title:'Burger k', value:28},
-    {date: new Date(2022, 7, 6), category:'rent', title:'Aluguel', value:2300},
-    {date: new Date(2022, 8, 18), category:'salary', title:'Salario', value: 4500},
 ];
